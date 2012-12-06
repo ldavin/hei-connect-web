2b1c3220b1549c895cdeee9256c65ceec921bece
Louis Davin <louis.developpement@gmail.com>
2012-12-06 15:43:00 +0100

    Refactor the layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def public_nav_link(text, link)
+    recognized = Rails.application.routes.recognize_path link
+    if recognized[:controller] == params[:controller] && recognized[:action] == params[:action]
+      content_tag :li, :class => "active" do
+        link_to text, link
+      end
+    else
+      content_tag :li do
+        link_to text, link
+      end
+    end
+  end
+
+  def app_nav_link(text, icon, link)
+    recognized = Rails.application.routes.recognize_path link
+    if recognized[:controller] == params[:controller] && recognized[:action] == params[:action]
+      content_tag :li, class: "active" do
+        link_to raw(content_tag(:i, nil, class: "icon-#{icon}") + text), link
+      end
+    else
+      content_tag :li do
+        link_to raw(content_tag(:i, nil, class: "icon-#{icon}") + text), link
+      end
+    end
+  end
 end
