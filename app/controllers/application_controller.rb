8037772b55a31a562fe6dbd94a7d32616d626dff
Louis Davin <louis.developpement@gmail.com>
2012-12-10 11:27:45 +0100

    Handle the case where the current user has been deleted and still logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||=
+        begin
+          User.find(session[:user_id]) if session[:user_id]
+        rescue
+          session[:user_id] = nil
+        end
   end
 
   def user_logged_in
