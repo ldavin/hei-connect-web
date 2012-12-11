c78fee0cf1ffe148096d981e71e206f2d7ca5b97
Louis Davin <louis.developpement@gmail.com>
2012-12-11 13:54:15 +0100

    Add a worker for fetching the user's schedule

---
+++ app/workers/fetch_schedule_worker.rb
@@ -0,0 +1,37 @@
+class FetchScheduleWorker
+  include Sidekiq::Worker
+
+  def perform(user_id)
+    user = User.find(user_id)
+
+    if user.schedule_planned?
+      data = JSON.parse(HeiConnect.get_weeks 'v1', user)
+
+      if data.include? 'error'
+        raise data['error']
+      end
+
+      data['weeks'].each do |week|
+        week_db = Week.find_or_create_by_user_id_and_number(user.id, week['number'])
+        week_db.update_attribute :rev, week_db.rev + 1
+
+        week['courses'].each do |course|
+          course_db = Course.where(date: Time.zone.parse(course['date']), length: course['length'],
+                                   kind: unescape(course['type']), group: unescape(course['group']),
+                                   code: unescape(course['code']), name: unescape(course['name']),
+                                   room: unescape(course['room']), teacher: unescape(course['teacher']),
+                                   week_id: week_db.id).first_or_create
+          course_db.update_attribute :week_rev, week_db.rev
+        end
+      end
+
+      user.schedule_ok!
+    end
+  end
+
+  private
+
+  def unescape(val)
+    val.nil? ? nil : URI.unescape(val)
+  end
+end
