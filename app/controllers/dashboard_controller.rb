545427039df33e7eda241ef56efea8d35986b341
Louis Davin <louis.developpement@gmail.com>
2012-12-14 15:08:56 +0100

    Automatically request a schedule update on first login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,14 @@ class DashboardController < ApplicationController
   before_filter :require_login
 
   def index
+    # First login check
+    if current_user.schedule_unknown?
+      current_user.schedule_planned!
+      FetchScheduleWorker.perform_async current_user.id
+    end
   end
 
   def courses
+    @weeks = current_user.weeks
   end
 end
