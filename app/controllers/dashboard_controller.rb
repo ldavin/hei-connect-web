6d834f0669f9b8cc7881ad2df973e95443acb32d
Louis Davin <louis.developpement@gmail.com>
2012-12-15 18:58:56 +0100

    Automatically check user data and fetch only 3 last notes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,8 @@
 class DashboardController < ApplicationController
-  before_filter :require_login
+  before_filter :require_login, :refesh_user_data
 
   def index
-    # First login check
-    if current_user.schedule_unknown?
-      current_user.schedule_planned!
-      FetchScheduleWorker.perform_async current_user.id
-    end
-
-    @notes = Note.order('updated_at DESC').limit 5
+    @notes = Note.order('updated_at DESC').limit 3
   end
 
   def courses
