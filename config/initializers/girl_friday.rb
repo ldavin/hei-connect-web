27d71d4db113d87df19ae93166c6a4d6e70907f7
Louis Davin <louis.developpement@gmail.com>
2012-12-16 22:40:19 +0100

    Switch to Redis To Go

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -1,11 +1,13 @@
 require 'connection_pool'
 
-if ENV['OPENSHIFT_GEAR_DIR']
-  redis = Redis.new path: "#{ENV['OPENSHIFT_GEAR_DIR']}tmp/redis.sock"
+if ENV['OPENSHIFT_APP_NAME']
+  # Fixme: Unsafe. No way on openshift to create private environment variables ?
+  uri = URI.parse('redis://redistogo:5905c8e3a33df5e2cc90d61cb6d39ca5@spadefish.redistogo.com:9408/')
+  options = {host: uri.host, port: uri.port, password: uri.password}
 else
-  redis = Redis.new host: 'localhost', port: 6379
+  options = {host: 'localhost', port: 6379}
 end
-redis_pool = ConnectionPool.new(:size => 5, :timeout => 5) { redis }
+redis_pool = ConnectionPool.new(:size => 5, :timeout => 5) { Redis.new options }
 
 CHECK_CREDENTIALS_QUEUE = GirlFriday::WorkQueue.new(:validate_credentials, :size => 1,
                                                     :store => GirlFriday::Store::Redis,
