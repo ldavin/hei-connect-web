736449cf036fdc2dc177f837d21a3e44ace6729a
Louis Davin <louis.developpement@gmail.com>
2012-12-16 13:41:57 +0100

    Configure redis for girl_friday

--- config/initializers/girl_friday.rb
+++ config/initializers/girl_friday.rb
@@ -1,7 +1,22 @@
-CHECK_CREDENTIALS_QUEUE = GirlFriday::WorkQueue.new(:validate_credentials, :size => 1) do |msg|
+require 'connection_pool'
+
+if ENV['VCAP_SERVICES']
+  services = JSON.parse(ENV['VCAP_SERVICES'])
+  redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
+  redis = services[redis_key].first['credentials']
+  REDIS_CONF = {host: redis['hostname'], port: redis['port'], password: redis['password']}
+else
+  REDIS_CONF = {host: 'localhost', port: 6379}
+end
+redis_pool = ConnectionPool.new(:size => 5, :timeout => 5) { Redis.new REDIS_CONF }
+
+CHECK_CREDENTIALS_QUEUE = GirlFriday::WorkQueue.new(:validate_credentials, :size => 1,
+                                                    :store => GirlFriday::Store::Redis,
+                                                    :store_config => {:pool => redis_pool}) do |msg|
   CheckCredentialsWorker.perform msg[:user_id]
 end
 
-FETCH_SCHEDULE_QUEUE = GirlFriday::WorkQueue.new(:fetch_schedule, :size => 1) do |msg|
+FETCH_SCHEDULE_QUEUE = GirlFriday::WorkQueue.new(:fetch_schedule, :size => 1, :store => GirlFriday::Store::Redis,
+                                                 :store_config => {:pool => redis_pool}) do |msg|
   FetchScheduleWorker.perform msg[:user_id]
 end
