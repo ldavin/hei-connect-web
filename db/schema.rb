81498de157e52592d2f5eb8bf23d48dfaacd3665
Louis Davin <louis.developpement@gmail.com>
2013-01-24 11:53:23 -0500

    Migrate DB for sections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124044738) do
+ActiveRecord::Schema.define(:version => 20130124164629) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -82,6 +82,13 @@ ActiveRecord::Schema.define(:version => 20130124044738) do
   create_table "notes", :force => true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sections", :force => true do |t|
+    t.string   "name"
+    t.string   "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
