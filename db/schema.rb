f4f7fe036674815be99144276a31f2d23d9f5037
Louis Davin <louis.developpement@gmail.com>
2013-01-24 11:53:54 -0500

    Create groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124164629) do
+ActiveRecord::Schema.define(:version => 20130124165332) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -78,6 +78,12 @@ ActiveRecord::Schema.define(:version => 20130124164629) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "notes", :force => true do |t|
     t.string   "title"
