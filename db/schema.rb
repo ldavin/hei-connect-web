4a6325f32f1bb04aa4b9c2d12d41c9ef89ef18cd
Louis Davin <louis.developpement@gmail.com>
2013-01-24 11:55:20 -0500

    Create rooms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124165414) do
+ActiveRecord::Schema.define(:version => 20130124165455) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -88,6 +88,12 @@ ActiveRecord::Schema.define(:version => 20130124165414) do
   create_table "notes", :force => true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "rooms", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
