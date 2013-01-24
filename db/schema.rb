e8ccf31fd054ec6a4541b70416d483ced036d1ca
Louis Davin <louis.developpement@gmail.com>
2013-01-24 11:54:32 -0500

    Create teachers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124165332) do
+ActiveRecord::Schema.define(:version => 20130124165414) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -95,6 +95,12 @@ ActiveRecord::Schema.define(:version => 20130124165332) do
   create_table "sections", :force => true do |t|
     t.string   "name"
     t.string   "code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "teachers", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
