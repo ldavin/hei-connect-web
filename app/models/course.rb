0b6025ff02199752687dbb56899d3f098534a7ff
Louis Davin <louis.developpement@gmail.com>
2013-02-08 00:24:51 -0500

    Update the ics generation

--- app/models/course.rb
+++ app/models/course.rb
@@ -20,22 +20,49 @@ class Course < ActiveRecord::Base
   has_many :course_teachers, dependent: :delete_all
   has_many :teachers, through: :course_teachers
   has_many :course_users, dependent: :delete_all
+  has_many :users, through: :course_users
 
   belongs_to :section
   belongs_to :group
 
   attr_accessible :ecampus_id, :date, :length, :kind, :broken_name
 
-  #def to_ics
-  #  event = Icalendar::Event.new
-  #  event.start = self.date.to_datetime
-  #  event.end = (self.date + self.length.minutes).to_datetime
-  #  event.summary = self.name
-  #  event.description = [self.kind, self.group, self.teacher].join ', '
-  #  event.location = self.room
-  #  event.klass = "PUBLIC"
-  #  event.created = self.created_at
-  #  event.last_modified = self.updated_at
-  #  event
-  #end
+  scope :current_weeks, lambda { where("date >= ?", Time.zone.now.beginning_of_week).order("date ASC") }
+
+  def to_ics
+    event_name = if self.broken_name.present?
+                   self.broken_name
+                 else
+                   "#{self.kind} - #{self.section.name}"
+                 end
+
+    event_desc = if self.broken_name.present?
+                   "E-Campus ne donne pas plus d'informations."
+                 else
+                   desc = "#{self.kind} de #{self.section.code} (#{self.section.name})"
+                   if self.teachers.any?
+                     desc += "\r\nPar #{self.teachers.join ', '}."
+                   end
+
+                   desc
+                 end
+
+    event_place = if self.rooms.any?
+                    self.rooms.join ', '
+                  else
+                    "Lieu inconnu"
+                  end
+
+    event = Icalendar::Event.new
+
+    event.start = self.date.to_datetime
+    event.end = (self.date + self.length.minutes).to_datetime
+    event.summary = event_name
+    event.description = event_desc
+    event.location = event_place
+    event.klass = "PUBLIC"
+    event.created = self.created_at
+    event.last_modified = self.updated_at
+    event
+  end
 end
