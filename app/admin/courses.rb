26e0b9da47f0f78417f099b0c61b40791584f9ff
Louis Davin <louis.developpement@gmail.com>
2013-02-08 12:20:22 -0500

    Empty active admin for courses

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,12 +1,3 @@
 ActiveAdmin.register Course do
-  index do
-    column :id
-    column :name
-    column :date
-    column :length
-    column :week_rev
-    column :kind
-    column :updated_at
-    default_actions
-  end
+
 end
