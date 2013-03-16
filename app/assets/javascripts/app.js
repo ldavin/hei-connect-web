7bcf6a1d7657c3fa4f8483a01f7180186865a1bb
Louis Davin <louis.developpement@gmail.com>
2013-03-15 21:32:04 -0400

    Migrate new javascripts
    [#46040585]

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,20 @@
+//= require jquery
+//= require bootstrap
+//= require jquery.piety
+//= require jquery.uniform
+//= require jquery.knob
+//= require chosen.jquery
+
+//= require flot/jquery.flot
+//= require flot/jquery.flot.resize
+//= require flot/jquery.flot.pie
+
+//= require justgage/raphael.2.1.0.min
+//= require justgage/justgage
+
+//= require jquery-ui
+
+//= require fullcalendar
+
+//= require ie-hacks
+
