11566425b4539f40449ca41cdfb8047480daf643
Louis Davin <louis.developpement@gmail.com>
2013-03-19 20:48:52 -0400

    Add css shim, pre-processing and removes public css
    [Fixes #46040585]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ HeiConnectWeb::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( active_admin.js excanvas.js ie_fix.js )
+  config.assets.precompile += %w( active_admin.css.scss ie_fix.css.scss )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
