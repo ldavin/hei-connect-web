8d4cd74ba3b42c8f79df89343d9580a1513e47aa
Louis Davin <louis.developpement@gmail.com>
2013-04-16 17:29:38 -0400

    Fix bad exception tracking

--- app/workers/check_user_worker.rb
+++ app/workers/check_user_worker.rb
@@ -9,25 +9,33 @@ class CheckUserWorker < ApplicationWorker
   end
 
   def perform
-    checked_user = User.find @user_id
-
-    client = Client.new
     begin
-      # Try to fetch an existing user with these credentials
-      api_user = client.user @username, @password
-    rescue RocketPants::NotFound
-      # We catch the user not found, but let a "bad credentials" error pop up
-      # We try to create the user
-      api_user = client.new_user @username, @password
+      checked_user = User.find @user_id
+
+      client = Client.new
+      begin
+        # Try to fetch an existing user with these credentials
+        api_user = client.user @username, @password
+      rescue RocketPants::NotFound
+        # We catch the user not found, but let a "bad credentials" error pop up
+        # We try to create the user
+        api_user = client.new_user @username, @password
+      end
+
+      checked_user.ecampus_id = api_user.username
+      checked_user.token = api_user.token
+      checked_user.save!
+      checked_user.user_ok!
+
+      # User valid, retrieve its info as soon as possible
+      Delayed::Job.enqueue FetchScheduleWorker.new(checked_user.id), priority: ApplicationWorker::PR_FETCH_SCHEDULE
+      Delayed::Job.enqueue FetchSessionsWorker.new(checked_user.id, true), priority: ApplicationWorker::PR_FETCH_SESSIONS
+    rescue RocketPants::Unauthenticated
+      checked_user.user_failed!
     end
+  end
 
-    checked_user.ecampus_id = api_user.username
-    checked_user.token = api_user.token
-    checked_user.save!
-    checked_user.user_ok!
-
-    # User valid, retrieve its info as soon as possible
-    Delayed::Job.enqueue FetchScheduleWorker.new(checked_user.id), priority: ApplicationWorker::PR_FETCH_SCHEDULE
-    Delayed::Job.enqueue FetchSessionsWorker.new(checked_user.id, true), priority: ApplicationWorker::PR_FETCH_SESSIONS
+  def success(job)
+    # We do nothing!
   end
 end
