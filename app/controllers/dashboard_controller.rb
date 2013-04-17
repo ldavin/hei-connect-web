3eb5520f5af10fdfbecd48c3457a9a683ad2b526
Louis Davin <louis.developpement@gmail.com>
2013-04-16 22:08:50 -0400

    Update http caches and run all queries in controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,15 +4,20 @@ class DashboardController < ApplicationController
   before_filter :require_login, :check_ecampus_suffix, :update_user_activity
 
   def index
-    @courses = current_user.courses.current_weeks
     @session = current_user.main_session || UserSession.new
     @updates = [current_user.schedule_update, current_user.sessions_update]
+
     unless @session.new_record?
       @updates.push current_user.grades_update(@session.grades_session)
       @updates.push current_user.absences_update(@session.absences_session)
     end
 
-    if stale? current_user, public: false
+    if stale? last_modified: max_timestamp(@updates), public: false
+      @courses = current_user.courses.includes(:section, :group, :course_rooms, :course_teachers,
+                                               course_rooms: :room, course_teachers: :teacher)
+      @grades = @session.grades.includes(:exam)
+      @absences = @session.absences
+
       respond_to do |format|
         format.html
       end
@@ -20,10 +25,12 @@ class DashboardController < ApplicationController
   end
 
   def courses
-    @courses = current_user.courses.current_weeks
     @updates = [current_user.schedule_update]
 
-    if stale? current_user, public: false
+    if stale? last_modified: max_timestamp(@updates), public: false
+      @courses = current_user.courses.includes(:section, :group, :course_rooms, :course_teachers,
+                                               course_rooms: :room, course_teachers: :teacher)
+
       respond_to do |format|
         format.html
         format.json { render :json => @courses.collect { |c| c.to_fullcalendar_event } }
@@ -33,12 +40,21 @@ class DashboardController < ApplicationController
   end
 
   def grades
-    @session = get_sessions params[:year], params[:try]
+    @session = get_session params[:year], params[:try]
     @updates = current_user.sessions.collect { |s| current_user.grades_update(s.grades_session) }
+    @exams = @session.exams
+
+    if stale? last_modified: max_timestamp(@updates + @exams), public: false
+      @grades = @session.grades.includes(:exam, exam: :section)
+
+      respond_to do |format|
+        format.html
+      end
+    end
   end
 
   def update_grades
-    @session = get_sessions params[:year], params[:try]
+    @session = get_session params[:year], params[:try]
 
     # We keep a 2 hours safety
     if current_user.grades_scheduled?(@session.grades_session)
@@ -55,12 +71,20 @@ class DashboardController < ApplicationController
   end
 
   def absences
-    @session = get_sessions params[:year], params[:try]
+    @session = get_session params[:year], params[:try]
     @updates = current_user.sessions.collect { |s| current_user.absences_update(s.absences_session) }
+
+    if stale? last_modified: max_timestamp(@updates), public: false
+      @absences = @session.absences.includes(:section)
+
+      respond_to do |format|
+        format.html
+      end
+    end
   end
 
   def update_absences
-    @session = get_sessions params[:year], params[:try]
+    @session = get_session params[:year], params[:try]
 
     # We keep a 2 hours safety
     if current_user.absences_scheduled?(@session.absences_session)
@@ -78,12 +102,17 @@ class DashboardController < ApplicationController
 
   private
 
-  def get_sessions(year, try)
-    begin
-      current_user.sessions.where(year: year.to_i, try: try.to_i).first!
-    rescue
-      redirect_to dashboard_grades_path(ecampus_id: current_user.ecampus_id, year: current_user.main_session.year,
-                                        try: current_user.main_session.try)
+  def max_timestamp(updates)
+    updates.collect { |u| u.updated_at }.max
+  end
+
+  def get_session(year, try)
+    result = current_user.sessions.select { |s| s.year == year.to_i and s.try == try.to_i }.first
+
+    if result.nil?
+      redirect_to dashboard_url ecampus_id: current_user.ecampus_id
+    else
+      result
     end
   end
 end
