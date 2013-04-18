b09b2970c24551757e4007843d07324909b8ed4e
Louis Davin <louis.developpement@gmail.com>
2013-04-18 17:03:42 -0400

    Load env vars only on production

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,2 +1,5 @@
 # Stream delayed_job logs to logentries
-Delayed::Worker.logger = Le.new(YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))['HEI_CONNECT_LOG_ENTRIES_DJ_KEY'])
+if Rails.env.production?
+  Delayed::Worker.logger = Le.new(
+      YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))['HEI_CONNECT_LOG_ENTRIES_DJ_KEY'])
+end
--- config/initializers/read_hei_connect_env_vars.rb
+++ config/initializers/read_hei_connect_env_vars.rb
@@ -1,4 +1,6 @@
 # Ugly workaround to load our env variables in openshift
 
 # Source the keys
-HC_ENV_KEYS = YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))
+if Rails.env.production?
+  HC_ENV_KEYS = YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))
+end
