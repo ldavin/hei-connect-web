aa7318a4d0829e1b10662c317e5d25f23db61e05
Louis Davin <louis.developpement@gmail.com>
2013-04-18 10:42:39 -0400

    Fix user env var retrieval

--- config/initializers/read_hei_connect_env_vars.rb
+++ config/initializers/read_hei_connect_env_vars.rb
@@ -1,8 +1,4 @@
-# Ugly workaround to create global env variables in openshift
+# Ugly workaround to load our env variables in openshift
 
 # Source the keys
-keys = YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))
-
-# Set the keys
-ENV['HEI_CONNECT_LOG_ENTRIES_KEY'] = keys['HEI_CONNECT_LOG_ENTRIES_KEY']
-ENV['HEI_CONNECT_NEW_RELIC_LICENCE_KEY'] = keys['HEI_CONNECT_NEW_RELIC_LICENCE_KEY']
+HC_ENV_KEYS = YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))
