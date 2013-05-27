00048070acf65ffc2b0f8e49ea6a4cc91df57e1d
Louis Davin <louis.developpement@gmail.com>
2013-05-27 11:46:00 +0200

    Remove logentries

--- config/initializers/delayed_job.rb
+++
@@ -1,5 +0,0 @@
-# Stream delayed_job logs to logentries
-if Rails.env.production?
-  Delayed::Worker.logger = Le.new(
-      YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))['HEI_CONNECT_LOG_ENTRIES_DJ_KEY'])
-end
