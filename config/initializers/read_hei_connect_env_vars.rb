42984a38c856c3d982fa0ea2f3446ff72c2ad425
Louis Davin <louis.developpement@gmail.com>
2013-06-15 15:42:00 +0200

    Delete openshift specific files

--- config/initializers/read_hei_connect_env_vars.rb
+++
@@ -1,6 +0,0 @@
-# Ugly workaround to load our env variables in openshift
-
-# Source the keys
-if Rails.env.production?
-  HC_ENV_KEYS = YAML.load(File.read("#{ENV['OPENSHIFT_DATA_DIR']}/keys.yaml"))
-end
