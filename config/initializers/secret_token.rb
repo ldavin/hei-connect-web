ed9a5f90b635a1cdf60f85171b4e348950a6c3eb
Louis Davin <louis.developpement@gmail.com>
2013-06-15 15:42:39 +0200

    Update env keys

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,1 @@
-require File.join(Rails.root, 'lib', 'openshift_secret_generator.rb')
-
-HeiConnectWeb::Application.config.secret_token = initialize_secret(
-    :token,
-    'irKjbnpf6ALDpkcEvaa6mnyeTuGBYzXiBHRjJUbmjBqBBj9HTek8LzDtNDeCUxzE7dRBsfbPEWLcPbnkcZTKNpsz4rDgXiJNGvUa'
-)
+HeiConnectWeb::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
