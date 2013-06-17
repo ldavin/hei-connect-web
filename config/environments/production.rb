ed22b77ab4499e9245999a4615efbcd77a6926e3
Louis Davin <louis.developpement@gmail.com>
2013-06-17 22:36:44 +0200

    Remove old files from active admin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ HeiConnectWeb::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( app.js public.js active_admin.js excanvas.js ie_fix.js )
-  config.assets.precompile += %w( app.css public.css active_admin.css active_admin/print.css ie_fix.css )
+  config.assets.precompile += %w( app.js public.js excanvas.js ie_fix.js )
+  config.assets.precompile += %w( app.css public.css ie_fix.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
