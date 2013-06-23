042344fa61f424ac36471a1b3843d37034000420
Louis Davin <louis.developpement@gmail.com>
2013-06-23 18:56:39 +0200

    Add coveralls and tune travis conf

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :test do
   gem 'capybara'
   gem 'guard-rspec'
   gem 'launchy'
+  gem 'coveralls'
 end
 
 gem 'jquery-rails'
--- spec/factories/absence.rb
+++ spec/factories/absence.rb
@@ -4,9 +4,9 @@ require 'faker'
 FactoryGirl.define do
   factory :absence do |f|
     f.date { DateTime.now }
-    f.length { 1 + Random.rand(90) }
+    f.length { 90 }
     f.excused { [true, false].sample }
     f.justification { Faker::Lorem.words.join ' ' }
-    f.update_number { 1 + Random.rand(90) }
+    f.update_number { Random.rand(10) }
   end
 end
