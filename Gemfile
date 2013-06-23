45bcf6379ba129134a0c45165323cf7e1edc0877
Louis Davin <louis.developpement@gmail.com>
2013-06-23 19:12:43 +0200

    Remove faker gem

--- Gemfile
+++ Gemfile
@@ -31,7 +31,6 @@ group :development, :test do
   gem 'factory_girl_rails'
 end
 group :test do
-  gem 'faker'
   gem 'capybara'
   gem 'guard-rspec'
   gem 'launchy'
--- spec/factories/absence.rb
+++ spec/factories/absence.rb
@@ -1,12 +1,11 @@
 # spec/factories/contacts.rb
-require 'faker'
 
 FactoryGirl.define do
   factory :absence do |f|
     f.date { DateTime.now }
-    f.length { 90 }
-    f.excused { [true, false].sample }
-    f.justification { Faker::Lorem.words.join ' ' }
-    f.update_number { Random.rand(10) }
+    f.length 90
+    f.excused false
+    f.justification 'Avait mal aux pieds'
+    f.update_number 3
   end
 end
