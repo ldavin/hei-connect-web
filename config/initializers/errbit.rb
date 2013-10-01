5184f1a04302423ad565954131fcf33742640e48
Louis Davin <ldavin@octo.com>
2013-10-01 23:02:11 +0200

    Fix airbrake conf

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_API_KEY']
+  config.api_key = "#{ENV['ERRBIT_API_KEY']}"
   config.host    = 'hei-connect-error.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
