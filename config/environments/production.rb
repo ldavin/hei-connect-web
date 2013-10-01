80e0c0da54474973b1ae7f14900450fa861583fb
Louis Davin <louis.developpement@gmail.com>
2013-10-01 23:02:03 +0200

    Add requires for loggly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require 'logglier'
+
 HeiConnectWeb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
+require 'logglier'
+
 if Rails.env.production?
   Resque.redis = Redis.new({host: ENV['REDIS_HOST'], db: ENV['REDIS_DB_JOBS']})
   Resque.logger = Logglier.new(ENV['LOGGLY_RAILS'])
