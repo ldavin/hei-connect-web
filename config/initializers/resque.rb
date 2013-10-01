3f00d911d117293ef5f62f3dd15236ea54631c0d
Louis Davin <ldavin@octo.com>
2013-10-01 23:02:10 +0200

    Remove shitty logglier and add airbrake

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,3 @@
-require 'logglier'
-
 if Rails.env.production?
   Resque.redis = Redis.new({host: ENV['REDIS_HOST'], db: ENV['REDIS_DB_JOBS']})
-  Resque.logger = Logglier.new(ENV['LOGGLY_RAILS'], :threaded => true)
 end
