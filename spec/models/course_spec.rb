b3ca4f21139ecba64c2bf9c06b30fb5b000e1139
Louis Davin <louis.developpement@gmail.com>
2013-10-24 21:15:33 +0200

    Refactor course tests

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -18,113 +18,85 @@
 require 'spec_helper'
 
 describe Course do
+  let(:section)  { create :section, name: 'Chimie organique' }
+  let(:rooms)    { [ create(:room, name: 'AC602') ] }
+  let(:teachers) { [ create(:teacher, name: 'Jean Mi') ] }
+  let(:course)   { create :course, kind: 'Cours', section: section, rooms: rooms, teachers: teachers }
 
-  let(:course) { FactoryGirl.create :course, kind: 'Cours' }
+  describe '#to_ical_event' do
+    let(:expected_event) {
+      event = RiCal.Event
+      event.dtstart = course.date
+      event.dtend = course.date + course.length.minutes
+      event.summary = 'Cours - Chimie organique'
+      event.description = "Cours de Chimie organique\r\nPar Jean Mi."
+      event.location = 'AC602'
+      event.created = course.created_at.to_datetime
+      event.last_modified = course.updated_at.to_datetime
 
-  it 'should generate an ical event' do
-    course.section = FactoryGirl.build :section, name: 'Chimie organique'
-    course.rooms = [ FactoryGirl.build(:room, name: 'AC602') ]
-    course.teachers = [ FactoryGirl.build(:teacher, name: 'Jean Mi') ]
+      event
+    }
 
-    expected_event = RiCal.Event
-    expected_event.dtstart = course.date
-    expected_event.dtend = course.date + course.length.minutes
-    expected_event.summary = 'Cours - Chimie organique'
-    expected_event.description = "Cours de Chimie organique\r\nPar Jean Mi."
-    expected_event.location = 'AC602'
-    expected_event.created = course.created_at.to_datetime
-    expected_event.last_modified = course.updated_at.to_datetime
+    subject { course.to_ical_event }
 
-    course.to_ical_event.should == expected_event
+    it { should eq expected_event }
   end
 
-  it 'should generate a full_calendar event' do
-    course.section = FactoryGirl.build :section, name: 'Electrotechnique'
-    course.rooms = [ FactoryGirl.build(:room, name: 'B216') ]
+  describe '#to_fullcalendar_event' do
+    let(:expected_hash) { { id: course.id, title: 'Cours - Chimie organique, AC602', start: course.date,
+                            end: course.date + course.length.minutes, allDay: false } }
+    subject { course.to_fullcalendar_event }
 
-    expected_hash = {
-                         id: course.id,
-                         title: 'Cours - Electrotechnique, B216',
-                         start: course.date,
-                         end: course.date + course.length.minutes,
-                         allDay: false
-                     }
-
-    course.to_fullcalendar_event.should == expected_hash
+    it { should eq expected_hash }
   end
 
-  describe 'should know its name' do
-    context 'when the data is incomplete' do
-      it 'should return the broken name' do
-        course.broken_name = 'Ecampus evil event'
+  describe '#name' do
+    subject { course.name }
 
-        course.name.should == 'Ecampus evil event'
-      end
+    context 'when there is a broken name' do
+      let(:course) { build :course, broken_name: 'cassé' }
+      it { should eq 'cassé' }
     end
 
-    context 'when the data is complete' do
-      it 'should concatenate the kind, section and teachers name' do
-        course.section = FactoryGirl.build :section, name: 'Automatique séquentielle'
-
-        course.name.should == 'Cours - Automatique séquentielle'
-      end
+    context 'when there is NOT a broken name' do
+      it { should eq 'Cours - Chimie organique' }
     end
   end
 
+  describe '#description' do
+    subject { course.description }
 
-  describe 'should be able to describe itself' do
-    context 'when the data is incomplete' do
-      it 'should return unknown' do
-        course.broken_name = 'Ecampus evil event'
-
-        course.description.should == Course::UNKNOWN_DESCRIPTION
-      end
+    context 'when there is a broken name' do
+      let(:course) { build :course, broken_name: 'cassé' }
+      it { should eq Course::UNKNOWN_DESCRIPTION }
     end
 
-    context 'when the data is complete' do
-      context 'when there are teachers' do
-        it 'should return everything concatenated' do
-          course.section = FactoryGirl.build :section, name: 'Mathématiques'
-          course.teachers = [ FactoryGirl.build(:teacher, name: 'Bibi'), FactoryGirl.build(:teacher, name: 'Bobo') ]
+    context 'when there are teachers' do
+      let(:teachers) { [ build(:teacher, name: 'Bibi'), build(:teacher, name: 'Bobo') ] }
+      it { should eq "Cours de Chimie organique\r\nPar Bibi, Bobo." }
+    end
 
-          course.description.should == "Cours de Mathématiques\r\nPar Bibi, Bobo."
-        end
-      end
-
-      context 'when there are no teachers' do
-        it 'should concatenate the kind and section name' do
-          course.section = FactoryGirl.build :section, name: 'Mathématiques'
-
-          course.description.should == 'Cours de Mathématiques'
-        end
-      end
+    context 'when there are NO teachers' do
+      let(:course) { build :course, section: section }
+      it { should eq 'Cours de Chimie organique' }
     end
   end
 
-  describe 'should know where it takes place' do
+  describe '#place' do
+    subject { course.place }
+
     context 'when in a single room' do
-      it 'should return the room name' do
-        course.rooms = [ FactoryGirl.build(:room, name: 'C416') ]
-
-        course.place.should == 'C416'
-      end
+      it { should eq 'AC602' }
     end
 
     context 'when in several rooms' do
-      it 'should concatenate the rooms names' do
-        course.rooms = [ FactoryGirl.build(:room, name: 'RR131'), FactoryGirl.build(:room, name: 'RR132') ]
-
-        course.place.should == 'RR131, RR132'
-      end
+      let(:rooms) { [ build(:room, name: 'RR131'), build(:room, name: 'RR132') ] }
+      it { should eq 'RR131, RR132' }
     end
 
-    context 'when in no rooms' do
-      it 'should return unknown' do
-        course.rooms = []
-
-        course.place.should == Course::UNKNOWN_PLACE
-      end
+    context 'when in NO rooms' do
+      let(:rooms) { [] }
+      it { should eq Course::UNKNOWN_PLACE }
     end
   end
-
 end
