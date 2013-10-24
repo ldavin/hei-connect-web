98518e7d60d0a322f00fb02e974a1d20e53f818e
Louis Davin <louis.developpement@gmail.com>
2013-10-24 20:44:17 +0200

    Test to_detailed_hash on absence

--- spec/models/absence_spec.rb
+++ spec/models/absence_spec.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # == Schema Information
 #
 # Table name: absences
@@ -17,7 +19,6 @@
 require 'spec_helper'
 
 describe Absence do
-
   let(:absence) { build :absence }
 
   describe '#type' do
@@ -37,5 +38,19 @@ describe Absence do
       let(:absence) { build :absence, excused: false, justification: '' }
       it { should eq Absence::TYPE_NOTHING }
     end
+  end
+
+  describe '#to_detailed_hash' do
+    let(:section) { create :section, name: 'Chimie' }
+    let(:absence) { build :absence, section_id: section.id, date: DateTime.new(2013, 10, 24, 11, 00, 00, +7),
+                                    length: 90, excused: true, justification: 'justif'}
+
+    subject { absence.to_detailed_hash }
+
+    its([:matiere]) { should eq 'Chimie' }
+    its([:date]) { should eq '24/10/2013 13h00' }
+    its([:duree]) { should eq 90 }
+    its([:type]) { should eq 'Excusée' }
+    its([:raison]) { should eq 'justif' }
   end
 end
