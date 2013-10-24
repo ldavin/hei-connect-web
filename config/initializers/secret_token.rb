5a39c2e3108b8d1470a39e17d8381086aba101cb
Louis Davin <louis.developpement@gmail.com>
2013-10-24 19:52:32 +0200

    Ignore secret token on dev or test environments

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,8 @@
-HeiConnectWeb::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
+token = case Rails.env
+          when 'development', 'test'
+            "123456789012345678901234567890"
+          else
+            ENV['APP_SECRET_TOKEN']
+        end
+
+HeiConnectWeb::Application.config.secret_token = token
