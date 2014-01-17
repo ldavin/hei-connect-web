112ffddbe328ffc868cb08b307594ff09496d13d
Louis Davin <louis.developpement@gmail.com>
2014-01-17 21:34:42 +0100

    Add a new case in the cleaner

--- app/workers/clean_accounts_scheduler.rb
+++ app/workers/clean_accounts_scheduler.rb
@@ -5,6 +5,7 @@ class CleanAccountsScheduler
   def self.perform *args
     now = DateTime.now
     User.where(token: nil).where('created_at < ?', now - 5.minutes).destroy_all
+    User.where(last_activity: nil).destroy_all
   end
 
 end
