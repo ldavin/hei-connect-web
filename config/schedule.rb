d57e0196b39d8641f3259c73ab8646697023ec02
Louis Davin <louis.developpement@gmail.com>
2014-02-09 23:07:07 +0100

    Start and stop jobs on deployment

--- config/schedule.rb
+++ config/schedule.rb
@@ -35,6 +35,12 @@ every 1.day, at: '2:00pm' do
   runner "Delayed::Job.enqueue UpdateSchedulesScheduler.new, priority: ApplicationWorker::PR_HIGHEST, queue: ApplicationWorker::QUEUE_REGULAR"
 end
 
+every 2.day, at: '11:00pm' do
+  system "script/delayed_job stop"
+  system "script/delayed_job --queue=regular -i=1 start"
+  system "script/delayed_job --queues=regular,grades -i=2 start"
+end
+
 every :sunday do
   runner "Delayed::Job.enqueue CleanCoursesScheduler.new, priority: ApplicationWorker::PR_HIGHEST, queue: ApplicationWorker::QUEUE_REGULAR"
 end
