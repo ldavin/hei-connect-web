608cb3707268de365f5a7f512543199b4562376f
Louis Davin <louis.developpement@gmail.com>
2014-07-22 20:35:02 +0200

    Remove old stuff (was used when self-hosting)

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-# Use Redis for sessions instead of the cookie-based default
-if ENV['WEB_SERVER'] == "true"
-	require 'action_dispatch/middleware/session/dalli_store'
-	HeiConnectWeb::Application.config.session_store :dalli_store, :memcache_server => 'localhost', :namespace => 'sessions', 
-													:key => '_foundation_session', :expire_after => 20.minutes
-end
--- config/puma.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env puma
-
-pidfile "/home/deployer/apps/hei-connect-web/tmp/puma/pid"
-state_path "/home/deployer/apps/hei-connect-web/tmp/puma/state"
-activate_control_app
