5eb4fca6524930c8806a9a238cab3bc987001850
Louis Davin <louis.developpement@gmail.com>
2014-08-18 09:27:31 +0200

    Only clean courses on sundays

--- app/workers/clean_courses_scheduler.rb
+++ app/workers/clean_courses_scheduler.rb
@@ -1,9 +1,12 @@
 class CleanCoursesScheduler
 
   def perform
-    date = Date.today
-    date = date - date.wday + 1
-    Course.where('date < ?', date).destroy_all
+    # Only clean courses on sundays
+    if Date.today.wday == 0
+      date = Date.today
+      date = date - date.wday + 1
+      Course.where('date < ?', date).destroy_all
+    end
   end
 
 end
