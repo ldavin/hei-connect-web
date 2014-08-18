17c395ad1087dabaeb75edb898228ccfd94075d5
Louis Davin <louis.developpement@gmail.com>
2014-08-18 09:23:21 +0200

    Only update sessions on sundays

--- app/workers/update_sessions_scheduler.rb
+++ app/workers/update_sessions_scheduler.rb
@@ -1,9 +1,12 @@
 class UpdateSessionsScheduler
 
   def perform
-    User.find_each(include: :updates) do |user|
-      if user.user_ok?
-        Delayed::Job.enqueue FetchSessionsWorker.new(user.id, false), priority: ApplicationWorker::PR_FETCH_SESSIONS, queue: ApplicationWorker::QUEUE_REGULAR
+    # Only schedule session updates on sundays
+    if Date.today.wday == 0
+      User.find_each(include: :updates) do |user|
+        if user.user_ok?
+          Delayed::Job.enqueue FetchSessionsWorker.new(user.id, false), priority: ApplicationWorker::PR_FETCH_SESSIONS, queue: ApplicationWorker::QUEUE_REGULAR
+        end
       end
     end
   end
