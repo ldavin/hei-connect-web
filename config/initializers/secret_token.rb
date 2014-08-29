327a706d69884fb7dd937c8e0ee58f3665c14693
Louis Davin <louis.developpement@gmail.com>
2014-08-29 09:43:22 +0200

    Set correct secret tokens

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-token = case Rails.env
-          when 'development', 'test'
-            "123456789012345678901234567890"
-          else
-            ENV['APP_SECRET_TOKEN']
-        end
-
-HeiConnectWeb::Application.config.secret_token = token
---
+++ config/initializers/secrets.rb
@@ -0,0 +1,8 @@
+case Rails.env
+  when 'development', 'test'
+    HeiConnectWeb::Application.config.secret_token = '123456789012345678901234567890'
+    HeiConnectWeb::Application.config.cookie_secret = '123456789012345678901234567890'
+  else
+    HeiConnectWeb::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
+    HeiConnectWeb::Application.config.cookie_secret = ENV['APP_COOKIE_KEY']
+end
