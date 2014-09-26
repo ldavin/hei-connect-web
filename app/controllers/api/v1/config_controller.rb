6bbedbe48695ef34eaf08dfaf75cdbf39fc5f1d0
Louis Davin <louis.developpement@gmail.com>
2014-09-26 08:55:28 +0200

    Bump minimal android version

--- app/controllers/api/v1/config_controller.rb
+++ app/controllers/api/v1/config_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::ConfigController < Api::ApiController
   def index
     @config = OpenStruct.new
     @config.url = 'https://hei-connect-web.herokuapp.com/'
-    @config.android_minimum_version = 1000
+    @config.android_minimum_version = 10001
   end
 end
