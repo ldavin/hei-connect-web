de0a29b97bf13a52793c0f11c73cd1fb1cf186c0
Stanislas Boyet <stanislas@boyet.me>
2015-06-07 19:51:29 +0200

    finishing up the guide

--- config/initializers/secrets.rb
+++ config/initializers/secrets.rb
@@ -2,7 +2,9 @@ case Rails.env
   when 'development', 'test'
     HeiConnectWeb::Application.config.secret_token = '123456789012345678901234567890'
     HeiConnectWeb::Application.config.cookie_secret = '123456789012345678901234567890'
+    HeiConnectWeb::Application.config.secret_key_base = '123456789012345678901234567890'
   else
     HeiConnectWeb::Application.config.secret_token = ENV['APP_SECRET_TOKEN']
     HeiConnectWeb::Application.config.cookie_secret = ENV['APP_COOKIE_KEY']
+    HeiConnectWeb::Application.config.secret_key_base = ENV['APP_COOKIE_KEY']
 end
