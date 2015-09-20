9f2641fe70014bf1e4fd6bb2711ddf636a48a2c9
Stanislas Boyet <stanislas@boyet.me>
2015-09-20 21:30:37 +0200

    fix transparent alert

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,7 +16,7 @@ class WelcomeController < ApplicationController
     end
     if user_logged_in
       if current_user.user_ok?
-        flash[:alert] = "Ca fait longtemps qu'on ne t'avait pas vu ! Pendant ton absence, on a arrêté de synchroniser tes données, mais tu es de retour dans les files d'attentes. Tes données toutes fraiches seront visibles dès demain." if (Time.now - 2.month) > current_user.last_activity
+        flash[:info] = "Ca fait longtemps qu'on ne t'avait pas vu ! Pendant ton absence, on a arrêté de synchroniser tes données, mais tu es de retour dans les files d'attentes. Tes données toutes fraiches seront visibles dès demain." if (Time.now - 2.month) > current_user.last_activity
         redirect_to dashboard_url ecampus_id: current_user.ecampus_id
       else
         redirect_to validate_users_url
